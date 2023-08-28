dc4194f22cbe3e8f0fadabed018294e988321b3f
Ana Julia de C Leandro <afloradda@gmail.com>
2023-08-28 14:55:24 -0300

    .

--- primeiro-java/src/BoletimEstudantil.java
+++ primeiro-java/src/BoletimEstudantil.java
@@ -1,6 +1,6 @@
 public class BoletimEstudantil {
     public static void main(String[] args) {
-        // sem identação
+        // sem indentação
         int mediaFinal = 7;
         if (mediaFinal<6)
         System.out.println("REPROVADO");
