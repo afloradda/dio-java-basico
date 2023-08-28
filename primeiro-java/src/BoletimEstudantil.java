855f7eaadcd6410e706dc1191ae073b3d0e8444f
Ana Julia de C Leandro <afloradda@gmail.com>
2023-08-28 14:53:26 -0300

    Entendendo indentação

---
+++ primeiro-java/src/BoletimEstudantil.java
@@ -0,0 +1,20 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        // sem identação
+        int mediaFinal = 7;
+        if (mediaFinal<6)
+        System.out.println("REPROVADO");
+        else if(mediaFinal==6)
+        System.out.println("PROVA MINERVA");
+        else
+        System.out.println("APROVADO");
+    }
+    
+}
+
+// para nivel de entendimento um código sem identação
+// é ruim principalmente pensando no compartilhamento
+// do mesmo.
+
+// com a tabulação conseguimos demonstrar que uma ação 
+// apenas é feita mediante a uma condição.
