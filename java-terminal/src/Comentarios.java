c838f6fc6d120c452103ce15a021252160bc5938
Ana Julia de C Leandro <afloradda@gmail.com>
2023-09-01 14:03:29 -0300

    Comentárioa

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,20 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Comentário de unica linha.
+
+        /*
+         * Comentário de multiplas linhas.
+         */
+    }
+
+    /**
+     * <h1>Apresentação</h1>
+     * Comentários com duas estrelas estão relacionados
+     * a aspectos do(s) método(s) utilizados.
+     * Sendo o mais utilizado para documentação.
+     */
+
+    public void metodo(){
+
+    }
+}
