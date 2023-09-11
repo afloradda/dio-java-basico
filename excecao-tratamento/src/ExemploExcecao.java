7eca38b0b13fcf666b061aeaa6b54d438f822ca4
Ana Julia de C Leandro <afloradda@gmail.com>
2023-09-11 14:30:43 -0300

    Exceção e Tratamento

---
+++ excecao-tratamento/src/ExemploExcecao.java
@@ -0,0 +1,18 @@
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class ExemploExcecao {
+    public static void main(String[] args) {
+        Number valor;
+        try{
+            valor = NumberFormat.getInstance().parse("a1.34");
+            System.out.println(valor);
+
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }
+
+        
+    }
+}
